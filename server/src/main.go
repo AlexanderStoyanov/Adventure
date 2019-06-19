45f20d6bb8f4536239f243dc045b5548047b8eef
Alexander Stoyanov <relax2865@gmail.com>
2019-06-19 17:07:43 -0400

    Register microservice is fully functional

--- server/src/main.go
+++ server/src/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"net/http"
 	"os"
-	"user"
+	"os/signal"
+	"syscall"
 
 	"cloud.google.com/go/firestore"
 	"github.com/go-kit/kit/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	// "github.com/AlexanderStoyanov/Adventure/server/register"
 	// "github.com/AlexanderStoyanov/Adventure/server/user"
@@ -14,16 +19,32 @@ import (
 	"register"
 )
 
+const (
+	defaultPort              = "8080"
+	defaultRoutingServiceURL = "http://localhost:7878"
+)
+
 func main() {
-	// Sets your Google Cloud Platform project ID.
-	projectID := "elliptical-city-243420"
+	var (
+		// Sets your Google Cloud Platform project ID.
+		projectID = "elliptical-city-243420"
+
+		addr = envString("PORT", defaultPort)
+		// rsurl = envString("ROUTINGSERVICE_URL", defaultRoutingServiceURL)
+
+		httpAddr = flag.String("http.addr", ":"+addr, "HTTP listen address")
+		// routingServiceURL = flag.String("service.routing", rsurl, "routing service URL")
+
+		ctx = context.Background()
+	)
 
 	// Get a Firestore client.
-	ctx := context.Background()
 	client, _ := firestore.NewClient(ctx, projectID)
 
 	// Close client when done.
 	defer client.Close()
+
+	flag.Parse()
 
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
@@ -33,6 +54,49 @@ func main() {
 
 	var rs register.Service
 	rs = register.NewService(repo, logger)
-	rs.RegisterUser(ctx, user.User{"Nastya", "san", "bla@bla123", "bla", 40.7128, -74.0060})
-	rs.GetUserByID(ctx, "san")
+
+	httpLogger := log.With(logger, "component", "http")
+	mux := http.NewServeMux()
+	mux.Handle("/register", register.MakeHandler(rs, httpLogger))
+
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+	go func() {
+		logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+		errs <- http.ListenAndServe(*httpAddr, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Log("terminated", <-errs)
+
+	//rs.RegisterUser(ctx, user.User{"Nastya", "san", "bla@bla123", "bla", 40.7128, -74.0060})
+	//rs.GetUserByID(ctx, "san")
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
 }
